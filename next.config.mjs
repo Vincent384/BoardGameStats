499dc35c460102fffd881381826825b3cfd66e8b
Vincent <vincentmu3@hotmail.com>
2024-11-27 16:36:50 +0100

    klar

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,9 @@
 import withPWA from "next-pwa";
 
-const pwaConfig = withPWA({
-  dest: "public", // Anger destinationen för PWA-relaterade filer
-});
-
-const nextConfig = pwaConfig({
-  // Andra Next.js-konfigurationer
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/firestore\.googleapis\.com\/.*/,
-      handler: "NetworkOnly", // Skippa caching för Firestore
-    },
-    // Lägg till andra caching-strategier här om behövs
-  ],
+const nextConfig = withPWA({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
 });
 
 export default nextConfig;
