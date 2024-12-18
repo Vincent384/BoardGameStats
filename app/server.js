2e2587e5fca346be19b4394536824b4faba825e8
Vincent <vincentmu3@hotmail.com>
2024-12-18 11:32:59 +0100

    klar

--- app/server.js
+++ app/server.js
@@ -8,6 +8,7 @@ export async function connectMongoDb() {
   if (isConnected) return; // Om redan ansluten, gör inget.
   try {
     console.log("Ansluter till MongoDB...");
+    console.log("MongoDB URI:", process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
